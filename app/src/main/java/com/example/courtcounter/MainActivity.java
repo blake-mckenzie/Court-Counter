9162f7ce6f7261563489e8c14a2e2b47c72992a7
Blake <02blake.mckenzie@gulllakecs.org>
2021-03-16 11:00:09 -0400

    L3.U21 - Added a Method for addThreePointsForTeamA

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+        displayForTeamA(0);
     }
+
+
+   public void addThreeForTeamA(View v){
+        displayForTeamA(3);
+   }
+
+
+
+
     /**
      * Displays the given score for Team A.
      */
